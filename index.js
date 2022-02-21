3161c5c212bb9cda9efe3a51f7709c52a2190b79
Aintzane Morales <aintzanemorales1992@gmail.com>
2022-02-21 19:20:39 +0100

    added middlewares and routes

--- index.js
+++ index.js
@@ -5,12 +5,22 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const helmet = require("helmet");
 const morgan = require("morgan");
-
+const userRoute= require ("./routes/users")
+const authRoute= require ("./routes/auth")
 dotenv.config();
 
 mongoose.connect(process.env.MONGO_URL, {useNewUrlParser: true}, () => {
     console.log("Connected to MongoDB")
 });
+
+//middleware
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"))
+
+
+app.use("/api/users", (userRoute))
+app.use("/api/auth", (authRoute))
 
 app.listen(5000, () => {
     console.log("Backend is ready to goooo!")

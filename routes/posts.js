23c5c4d89f0f5c2f7a18a7f16676248b3b94fd6e
Aintzane Morales <aintzanemorales1992@gmail.com>
2022-02-28 17:41:13 +0100

    minor changes

--- routes/posts.js
+++ routes/posts.js
@@ -88,17 +88,33 @@ router.get("/:id", async (req, res) => {
 //get timeline posts
 
 
-router.get("/timeline/all", async (req, res) => {
+router.get("/timeline/:userId", async (req, res) => {
    
     try {
-        const currentUser = await User.findById(req.body.userId);
+        const currentUser = await User.findById(req.params.userId);
         const userPost = await Post.find({ userId : currentUser._id});
         const friendsPosts = await Promise.all(
             currentUser.followings.map((friendId) => {
                 return Post.find({userId : friendId})
             })
         );
-        res.json(userPost.concat(...friendsPosts))
+        res.status(200).json(userPost.concat(...friendsPosts))
+
+    }catch(err) {
+        res.status(500).json(err)
+    }
+})
+
+
+//get user's all posts
+
+router.get("/profile/:username", async (req, res) => {
+   
+    try {
+        const user = await User.findOne({username : req.params.username})
+        const posts = await Post.find({userId: user._id})
+       
+        res.status(200).json(posts)
 
     }catch(err) {
         res.status(500).json(err)
